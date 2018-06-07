c9d3b956313b265f9e94f2aa28e51e58620ac780
GoneGame <doom_lol97@outlook.com>
2018-06-07 21:09:10 +0800

    edited index, dividing the login from the content, tried to make sidebar, in progress, changing login javascript in progress

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -1,25 +1,19 @@
 firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
     // User is signed in.
-
-    document.getElementById("user_div").style.display = "block";
-    document.getElementById("login_div").style.display = "none";
-
     var user = firebase.auth().currentUser;
 
     if(user != null){
 
       var email_id = user.email;
       document.getElementById("user_para").innerHTML = "Welcome " + email_id;
-
+	  window.location.href="everything.html";
+	  window.alert(user.username);
     }
 
   } else {
     // No user is signed in.
-
-    document.getElementById("user_div").style.display = "none";
-    document.getElementById("login_div").style.display = "block";
-
+	
   }
 });
 
