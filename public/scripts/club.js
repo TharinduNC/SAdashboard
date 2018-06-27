9921d55cf767fa3eff9b2653f9b1f5ed1545ae01
GoneGame <doom_lol97@outlook.com>
2018-06-27 21:26:38 +0800

    changed club.js to hold controllers related to clubs, added checkboxes in view for items

--- public/scripts/club.js
+++ public/scripts/club.js
@@ -1,34 +1,69 @@
-function clubCreate()
-{
-	var clubName = document.getElementById("club_name").value;
+app.controller('clubCreate', function($scope) {
 	
-	var firebaseRef = firebase.database().ref();
+	$scope.createClub = function(form)
+	{
+		if(form.$valid)
+		{
+			var firebaseRef = firebase.database().ref();
+		
+			firebaseRef.child("club").push().child("name").set($scope.clubName);
+		}
+		else
+		{
+			window.alert("empty");
+		}
+		
+	};
+});
+
+
+app.controller('clubRead', function($scope, $firebaseArray) {
+	//read more https://stackoverflow.com/questions/20181323/passing-data-between-controllers-in-angular-js
+	var firebaseRefClub = firebase.database().ref("club");
 	
-	if(clubName.length < 1)
-	{
-		window.alert("empty");
-	}
-	else
-	{
-		firebaseRef.child("club").push().child("name").set(clubName);
-	}
+	$scope.clubs = $firebaseArray(firebaseRefClub);
+});
+
+
+app.controller('clubEdit', function($scope, $firebaseArray) {
 	
-	/*
-	if(clubName.length < 1)
-	{
-		window.alert("empty");
-	}
-	else
-	{
-		window.alert(clubName);
-	}*/
+	var firebaseRefClub = firebase.database().ref("club");
 	
-}
+	$scope.clubs = $firebaseArray(firebaseRefClub);
+	
+	
+	
+});
+
+
 
 /*
-code for table looping duplicates
+****** if angular fire gets confusing, can use code below to get snapshot of club leaves ******
 
-https://www.w3schools.com/code/tryit.asp?filename=FSOMG40PE1MZ
+https://firebase.google.com/docs/database/web/lists-of-data
+
+
+function readClub()
+{	
+	var firebaseRefClub = firebase.database().ref("club");
+	
+	firebaseRefClub.on("value", function(snapshot) {
+		
+		snapshot.forEach(function(childSnapshot) {
+			var childKey = childSnapshot.key;
+			var childData = childSnapshot.val();
+			
+			getClub(childData);
+			//console.log(hello);
+			
+		});
+		
+	}, function(errorObject) {
+		console.log("the read failed: " + errorObject.code);
+		hello = errorObject;
+	});
+	
+}
 */
 
 
