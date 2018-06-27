ccb8b2843ed24b1f5179f16f88419c0243536af5
GoneGame <doom_lol97@outlook.com>
2018-06-27 20:35:35 +0800

    changed create club to angularjs

--- public/scripts/angular.js
+++ public/scripts/angular.js
@@ -1,11 +1,38 @@
 var app = angular.module('myApp', ["firebase"]);
 
+//check validity of input https://stackoverflow.com/questions/20054055/angularjs-check-if-form-is-valid-in-controller
+
+app.controller('clubCreate', function($scope) {
+	
+	$scope.createClub = function(form)
+	{
+		if(form.$valid)
+		{
+			var firebaseRef = firebase.database().ref();
+		
+			firebaseRef.child("club").push().child("name").set($scope.clubName);
+		}
+		else
+		{
+			window.alert("empty");
+		}
+		
+	};
+});
+
+
 app.controller('clubRead', function($scope, $firebaseArray) {
 	
 	var firebaseRefClub = firebase.database().ref("club");
 	
 	$scope.clubs = $firebaseArray(firebaseRefClub);
 });
+
+
+app.controller('clubEdit', function($scope, $firebaseArray) {
+});
+
+
 
 /*
 ****** if angular fire gets confusing, can use code below to get snapshot of club leaves ******
