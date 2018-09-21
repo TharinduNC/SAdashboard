fdd3cb67564a2578e4272c846e429f44525ffeed
GoneGame <doom_lol97@outlook.com>
2018-09-21 22:12:30 +0800

    unit edit selection works, editing the content itself is commented as it is not yet tested

--- public/scripts/unit.js
+++ public/scripts/unit.js
@@ -39,6 +39,58 @@ app.controller('unitAll', function($scope, $firebaseArray) {
 	};
 	
 	//update unit
-	//testing from laptop local disk
-	//is it GoneGame?
+	
+	$scope.onUnitEditClick = function() {
+		if($scope.unitChecked.units.length == 1)
+		{
+			var unitRef = firebase.database().ref("unit/" + $scope.unitChecked.units[0]);
+		
+			unitRef.on('value', function(snapshot) {
+				var currentUnit = snapshot.val();
+				$scope.currentUnitCode = currentUnit.code;
+				$scope.currentUnitName = currentUnit.name;
+			});
+		}
+		else
+		{
+			$scope.currentUnitCode = "<>"
+			$scope.currentUnitName = "<no selection or too many selected>";
+		}
+	};
+	
+	$scope.newUnitCode = "";
+	$scope.newUnitName = "";
+	
+	/*
+	//only one selection is allowed when updating the unit
+	$scope.updateUnit = function() {
+		
+		if($scope.unitChecked.units.length == 1)
+		{
+			if($scope.newUnitName.length > 0 && $scope.newUnitCode > 0)
+			{
+				window.alert($scope.newUnitCode + " " + $scope.newUnitName);
+			
+				var updatedUnit = {
+					code: $scope.newUnitCode,
+					name: $scope.newUnitName
+				};
+				
+				var updates = {};
+				updates['unit/' + $scope.unitChecked.units[0]] = updatedUnit;
+				
+				firebase.database().ref().update(updates);
+			}
+			else
+			{
+				window.alert("empty");
+			}
+		}
+		else
+		{
+			window.alert("no selection");
+		}
+	};*/
+	
+	
 });
