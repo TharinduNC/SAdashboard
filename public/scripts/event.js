83fac7a29ea873fa5866999962fee65f6bd9b9fb
GoneGame <doom_lol97@outlook.com>
2018-09-22 16:06:30 +0800

    event can be added, but relation is just a placeholder

---
+++ public/scripts/event.js
@@ -0,0 +1,36 @@
+app.controller('eventAll', function($scope, $firebaseArray) {
+	
+	var firebaseRefEvent = firebase.database().ref("event");
+	
+	var list = $firebaseArray(firebaseRefEvent);
+	
+	$scope.createEvent = function(form)
+	{
+		
+		if(form.$valid)
+		{
+			
+			list.$add({
+				date: $scope.eventDate,
+				desc: $scope.eventDesc,
+				location: $scope.eventLocation,
+				note: $scope.eventNote,
+				relateTo: "<unit/club>/uid",
+				time: $scope.eventTime,
+				title: $scope.eventTitle
+			}).then(function(ref) {
+				var id = ref.key;
+				//console.log("added record with id " + id);
+				window.alert("added record with id " + id);
+				list.$indexFor(id); // returns location in the array
+				
+				//try to clear the field after addition
+			});
+		}
+		else
+		{
+			window.alert("empty");
+		}
+	};
+	
+});
