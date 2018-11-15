dcbc8dd3619b72a10426dc771b78c8bea52a902e
gunatilake <gunatilake@protonmail.com>
2018-11-15 16:15:32 +0800

    fixed broken tags, removed repeated scripts, refreshed login page

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -21,14 +21,17 @@ function login(){
 	var userEmail = document.getElementById("email_field").value;
 	var userPass = document.getElementById("password_field").value;
 	window.alert("Hi");
-	firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
-    // Handle Errors here.
-	var errorCode = error.code;
-    var errorMessage = error.message;
-
-    window.alert("Error : " + errorMessage);
-
-    // ...
-  });
+	firebase.auth().signInWithEmailAndPassword(userEmail, userPass)
+			.catch(function(error) {
+		// Handle Errors here.
+		var errorCode = error.code;
+		var errorMessage = error.message;
+		if (errorCode === 'auth/wrong-password') {
+			alert('Wrong password.');
+		} else {
+			alert(errorMessage);
+		}
+		console.log(error);
+	});
 
 }
